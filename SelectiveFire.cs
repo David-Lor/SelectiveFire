504ac70dcec698082fa49d7a9b4ad1aef6917748
EnforcerZhukov <davidlorenzo@openmailbox.org>
2016-05-17 20:12:49 +0200

    Add files via upload

--- SelectiveFire.cs
+++ SelectiveFire.cs
@@ -10,8 +10,8 @@ public class SelectiveFire : Script
 {
     //LOCAL VARIABLES for the whole script
         private int fireMode = 1, //1= full auto, 2= semi-auto, 3=burst (3 shots)
-        ammo, prevammo, ammocount;
-        private bool capableWeapon = false, firemodechanged = false, showImg = false, stealth, stealthLaunchIfPlayerAiming = false, playerreloaded = false, shaking = false;
+        ammo, /*prevammo,*/ ammocount;
+        private bool capableWeapon = false, firemodechanged = false, showImg = false, stealth, stealthLaunchIfPlayerAiming = false, playerreloaded = false, breathshaking = false, aimshaking = false;
         private string firemodeImgRoot = AppDomain.CurrentDomain.BaseDirectory + "\\SelectiveFire\\", firemodeImg;
         private Weapon previousWeapon;
         private Ped player;
@@ -20,8 +20,8 @@ public class SelectiveFire : Script
     //VARIABLES FROM INI CONFIG FILE
         private ScriptSettings config;
         private Keys ChangeFireModeHotkey_Keys;
-        private bool ActivateSelectiveFire, ShowImage, AutoHideImage, StealthIfPlayerAiming, StealthAutoDisable, WasteAmmo, ShowNotif, BreathAimMovement, HeavyRecoil;
-        private int ImageShownTime, ImageWidth, ImageHeight, BreathMovementRate, RecoilRate;
+        private bool ActivateSelectiveFire, ShowImage, AutoHideImage, StealthIfPlayerAiming, StealthAutoDisable, WasteAmmo, ShowNotif, BreathAimMovement, ShakeWhenAim;
+        private int ImageShownTime, ImageWidth, ImageHeight, BreathMovementRate, ShakeWhenAimMovementRate;
 
     //FOR MOUSE CLICK SIMULATION
         [DllImport("user32.dll")]
@@ -51,8 +51,8 @@ public class SelectiveFire : Script
             WasteAmmo = config.GetValue<bool>("REALLISTICMAGS", "WasteAmmo", false);
             BreathAimMovement = config.GetValue<bool>("REALLISTICAIMING", "BreathAimMovement", false);
             BreathMovementRate = config.GetValue<int>("REALLISTICAIMING", "BreathMovementRate", 1);
-            HeavyRecoil = config.GetValue<bool>("REALLISTICAIMING", "HeavyRecoil", false);
-            RecoilRate = config.GetValue<int>("REALLISTICAIMING", "RecoilRate", 1);
+            ShakeWhenAim = config.GetValue<bool>("REALLISTICAIMING", "ShakeWhenAim", false);
+            ShakeWhenAimMovementRate = config.GetValue<int>("REALLISTICAIMING", "ShakeWhenAimMovementRate", 1);
 
 
         //Hotkeys (String from INI to Enum-Keys)
@@ -106,10 +106,6 @@ public class SelectiveFire : Script
 
             //Restart ammocount when player is reloading or has changed weapon (avoiding firing after that)
                 if (player.IsReloading || actualWeapon != previousWeapon) {
-                    /*if (ammocount < 3) {
-                        LeftMouseUp();
-                        Game.Player.DisableFiringThisFrame();
-                    }*/
                     Game.Player.DisableFiringThisFrame();
                     ammocount = 0;
                 }
@@ -120,19 +116,19 @@ public class SelectiveFire : Script
         }
 
         //STEALTH MODULE
-            if (StealthIfPlayerAiming) {
-                stealth = GTA.Native.Function.Call<bool>(GTA.Native.Hash.GET_PED_STEALTH_MOVEMENT, player);
-                if (StealthIfPlayerAiming && !stealthLaunchIfPlayerAiming && Game.Player.IsAiming) {
-                    StealthMode(true);
-                    stealthLaunchIfPlayerAiming = true; //just launch stealth order once
-                }
-                else if (stealthLaunchIfPlayerAiming && !Game.Player.IsAiming) {
-                    stealthLaunchIfPlayerAiming = false;
-                    if (StealthAutoDisable && stealth) { //auto-disable stealth option
-                        StealthMode(false);
-                    }
+        if (StealthIfPlayerAiming) {
+            stealth = GTA.Native.Function.Call<bool>(GTA.Native.Hash.GET_PED_STEALTH_MOVEMENT, player);
+            if (StealthIfPlayerAiming && !stealthLaunchIfPlayerAiming && Game.Player.IsAiming) {
+                StealthMode(true);
+                stealthLaunchIfPlayerAiming = true; //just launch stealth order once
+            }
+            else if (stealthLaunchIfPlayerAiming && !Game.Player.IsAiming) {
+                stealthLaunchIfPlayerAiming = false;
+                if (StealthAutoDisable && stealth) { //auto-disable stealth option
+                    StealthMode(false);
                 }
             }
+        }
 
         //REALLISTIC MAGS MODULE
         if (WasteAmmo && (actualWeapon.Hash == GTA.Native.WeaponHash.AdvancedRifle || actualWeapon.Hash == GTA.Native.WeaponHash.APPistol || actualWeapon.Hash == GTA.Native.WeaponHash.AssaultRifle || actualWeapon.Hash == GTA.Native.WeaponHash.AssaultSMG || actualWeapon.Hash == GTA.Native.WeaponHash.BullpupRifle || actualWeapon.Hash == GTA.Native.WeaponHash.CarbineRifle || actualWeapon.Hash == GTA.Native.WeaponHash.CombatMG || actualWeapon.Hash == GTA.Native.WeaponHash.CombatPDW || actualWeapon.Hash == GTA.Native.WeaponHash.CombatPistol || actualWeapon.Hash == GTA.Native.WeaponHash.CompactRifle || actualWeapon.Hash == GTA.Native.WeaponHash.GrenadeLauncher || actualWeapon.Hash == GTA.Native.WeaponHash.GrenadeLauncherSmoke || actualWeapon.Hash == GTA.Native.WeaponHash.Gusenberg || actualWeapon.Hash == GTA.Native.WeaponHash.HeavyPistol || actualWeapon.Hash == GTA.Native.WeaponHash.HeavyShotgun || actualWeapon.Hash == GTA.Native.WeaponHash.HeavySniper || actualWeapon.Hash == GTA.Native.WeaponHash.MachinePistol || actualWeapon.Hash == GTA.Native.WeaponHash.MarksmanRifle || actualWeapon.Hash == GTA.Native.WeaponHash.MG || actualWeapon.Hash == GTA.Native.WeaponHash.MicroSMG || actualWeapon.Hash == GTA.Native.WeaponHash.Pistol || actualWeapon.Hash == GTA.Native.WeaponHash.Pistol50 || actualWeapon.Hash == GTA.Native.WeaponHash.Revolver || actualWeapon.Hash == GTA.Native.WeaponHash.SMG || actualWeapon.Hash == GTA.Native.WeaponHash.SniperRifle || actualWeapon.Hash == GTA.Native.WeaponHash.SNSPistol || actualWeapon.Hash == GTA.Native.WeaponHash.SpecialCarbine || actualWeapon.Hash == GTA.Native.WeaponHash.VintagePistol || actualWeapon.Hash == GTA.Native.WeaponHash.AssaultShotgun)) 
@@ -148,8 +144,9 @@ public class SelectiveFire : Script
             }
         }
 
-        //HEAVYRECOIL MODULE
-        if (HeavyRecoil) { HeavyRecoilModule(); }
+        //REALLISTICAIMING MODULE
+        if (BreathAimMovement) { BreathAimModule(); }
+        if (ShakeWhenAim) { ShakeWhenAimModule(); }
 
         previousWeapon = actualWeapon;
     }
@@ -178,34 +175,29 @@ public class SelectiveFire : Script
 
     private void BurstMode()
     { //Script for Burst Fire Mode
-        if (prevammo - ammo == 1) {
-            ammocount += 1;
+        if (player.IsShooting) {
+            ammocount++;
         }
         if (ammocount == 1 || ammocount == 2) {
-            mouse_event(MOUSEEVENTF_LEFTDOWN, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+            Game.SetControlNormal(0, GTA.Control.Attack, 1.0f);
+            if (player.IsAimingFromCover) {
+                Game.SetControlNormal(0, GTA.Control.Aim, 1.0f);
+            }
         }
         if (ammocount == 3) {
-            LeftMouseUp();
-            ammocount = 0;
+            Game.Player.DisableFiringThisFrame();
+            if (GTA.Game.IsControlJustReleased(0, GTA.Control.Attack)) {
+                ammocount = 0;
+            }
         }
-        prevammo = ammo;
-    }
-
-    private void LeftMouseUp()
-    { //Script to simulate a left mouse click
-        mouse_event(MOUSEEVENTF_LEFTUP, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+        //prevammo = ammo;
     }
 
     private void SemiAutoMode()
     { //Script for Semi-Auto Fire Mode
-        if (prevammo - ammo == 1 && GTA.Game.IsControlPressed(1, GTA.Control.Attack)) {
-            //LeftMouseUp();
-            /*if (GTA.Game.IsControlPressed(420, GTA.Control.Attack)) { Game.Player.DisableFiringThisFrame(); UI.ShowSubtitle("controlpressedattack-semiauto"); }*/
+        if (GTA.Game.IsControlPressed(0, GTA.Control.Attack)) {
             Game.Player.DisableFiringThisFrame();
         }
-
-        if (Game.IsControlJustReleased(1, GTA.Control.Attack)) { prevammo = ammo; }
-
     }
 
     private void StealthMode(bool sth)
@@ -213,20 +205,26 @@ public class SelectiveFire : Script
         GTA.Native.Function.Call(GTA.Native.Hash.SET_PED_STEALTH_MOVEMENT, player, sth, 0);
     }
 
-    private void HeavyRecoilModule()
-    {
-        if (Game.Player.IsAiming && !shaking) { //SHAKING NORMAL... no creo que pueda cambiar "al vuelo" el float
-            //GameplayCamera.Shake(GTA.CameraShake.Jolt, (0.1f * BreathMovementRate)); //mover solo 1 vez... tembleque, pasa justo al apuntar un momentito
-                GameplayCamera.Shake(GTA.CameraShake.Hand, (0.1f * BreathMovementRate)); //mover camara respirar
-                shaking = true;
-            
+    private void BreathAimModule()
+    { //Shake "Hand" effect - when you're aiming the camera moves like if breathing
+        if (Game.Player.IsAiming && !breathshaking) {
+            GameplayCamera.Shake(GTA.CameraShake.Hand, (0.1f * BreathMovementRate));
+            breathshaking = true;
         }
-        else if (!Game.Player.IsAiming) {
+        else if (!Game.Player.IsAiming) { //avoid shake running more than once
             GameplayCamera.StopShaking();
-            shaking = false;
+            breathshaking = false;
         }
-        
-        
     }
 
+    private void ShakeWhenAimModule() 
+    { //Shake "Jolt" effect - just applies when start aiming, moving the camera
+        if (Game.Player.IsAiming && !aimshaking) {
+            GameplayCamera.Shake(GTA.CameraShake.Jolt, (0.1f * ShakeWhenAimMovementRate));
+            aimshaking = true;
+        }
+        else if (!Game.Player.IsAiming) { //avoid shake running more than once
+            aimshaking = false;
+        }
+    }
 }
